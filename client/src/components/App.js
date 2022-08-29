fb568ecea44a933319eb65b9ed38812054e96723
pablolucianop <pablolucianop@gmail.com>
2022-08-28 23:05:57 -0300

    feat: conect login and welcome

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,13 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import LoginContainer from './LoginContainer'
 
 function App() {
-  const [state, setState] = useState({ apiResponse: '' })
-  const [jwt, setJwt] = useState('')
-  const [apiRes, setApiRes] = useState({ apiResponse: '' })
-  const [validMail, setValidMail] = useState(false)
   const [emailValidated, setEmailValidated] = useState(false)
-  const [submitedMail, setSubmitedMail] = useState(false)
   const [userData, setUserData] = useState({ user: false })
 
   async function handleSubmitEmail(email) {
-    // setSubmitedMail(state)
-    return fetch('http://localhost:9000/validateMail', {
+    let res = await fetch('http://localhost:9000/validateMail', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -22,20 +16,13 @@ function App() {
         email: email,
       }),
     })
-      .then((data) => data.json())
-      .then((data) => setEmailValidated(data))
-      .then((data) => console.log('emailValidated.apiResponse', emailValidated))
-  }
-
-  function callAPI() {
-    fetch('http://localhost:9000/testAPI')
-      .then((res) => res.text())
-      .then((res) => setState({ apiResponse: res }))
+    let b = res.json()
+    setEmailValidated(b)
   }
 
   async function loginUser(email, password) {
     console.log('email, password', email, password)
-    return fetch('http://localhost:9000/login', {
+    let res = await fetch('http://localhost:9000/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -45,13 +32,13 @@ function App() {
         password: password,
       }),
     })
-      .then((data) => data.json())
-      .then((data) => setJwt(data))
-      .then((data) => console.log('jwt', jwt))
+    let b = await res.json()
+    welcome(b)
   }
 
-  async function welcome(email, password) {
-    return fetch('http://localhost:9000/welcome', {
+  async function welcome(jwt2) {
+    console.log('jwt2', jwt2.body)
+    let res = await fetch('http://localhost:9000/welcome', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -59,26 +46,12 @@ function App() {
       body: JSON.stringify({
         email: 'it@drixit.com',
         password: 'some-password',
-        token:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Iml0QGRyaXhpdC5jb20iLCJpYXQiOjE2NjE2OTUwOTgsImV4cCI6MTY2MTcwMjI5OH0.Pxeaed0L-O6_sEfE-t0QM82NupBPX5W8G4lHdE47i_g',
+        token: jwt2,
       }),
     })
-      .then((data) => data.json())
-      .then((data) => setUserData(data))
-      .then((data) => console.log('userData', userData))
+    let b = await res.json()
+    setUserData(b)
   }
-
-  // function getData() {
-  //   return fetch(`https://jsonplaceholder.typicode.com/`)
-  //     .then
-  //     // (response) => welcome()
-  //     ()
-  // }
-
-  // useEffect(() => {
-  //   getData().then()
-  //   console.log('useEffect ran...')
-  // }, [jwt])
 
   return (
     <div>
@@ -88,10 +61,8 @@ function App() {
         loginUser={loginUser}
         emailValidated={emailValidated}
       />
-      <button onClick={welcome}>welcome</button>
-      {userData.user !== false && (
-        <div onClick={welcome}>welcome!!!!!! {userData.name}</div>
-      )}
+      <button>welcome</button>
+      {userData.user !== false && <div>welcome!!!!!! {userData.name}</div>}
     </div>
   )
 }
