bc7960428ec036f12ce3d104ddb96a707055383e
pablolucianop <pablolucianop@gmail.com>
2022-09-03 11:49:25 -0300

    fix: change index for deploy

--- api/app.js
+++ api/app.js
@@ -9,10 +9,9 @@ const app = express()
 
 const path = require('path')
 app.use(express.static(path.join(__dirname, 'build')))
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', index.html))
-})
-// app2.listen(5000)
+// app.get('/ee', function (req, res) {
+//   res.sendFile(path.join(__dirname, './', 'build', index.html))
+// })
 
 require('dotenv').config()
 app.use(cors())
@@ -22,22 +21,7 @@ app.use('/users', usersRouter)
 app.use('/v0/authenticate', authenticateRouter)
 app.use('/v0/validateMail', validateMailRouter)
 app.use('/v0/users/me', usersMeRouter)
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404))
-})
 
 console.log('app')
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error')
-})
 
 module.exports = app
