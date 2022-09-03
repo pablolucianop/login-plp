ea87cfb79054ee942ffaae0eb488574f3c4b35eb
pablolucianop <pablolucianop@gmail.com>
2022-09-03 14:00:17 -0300

    fix: react router  in express

--- api/app.js
+++ api/app.js
@@ -9,9 +9,12 @@ const app = express()
 
 const path = require('path')
 app.use(express.static(path.join(__dirname, 'build')))
-// app.get('/ee', function (req, res) {
-//   res.sendFile(path.join(__dirname, './', 'build', index.html))
-// })
+
+app.get('*', function (req, res) {
+  res.sendFile('index.html', {
+    root: path.join(__dirname, 'build'),
+  })
+})
 
 require('dotenv').config()
 app.use(cors())
