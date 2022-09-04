bf85db49046da04bf34fe633ca388d40263f93e2
pablolucianop <pablolucianop@gmail.com>
2022-09-04 10:59:13 -0300

    fix: homepage for gh-pages

--- api/app.js
+++ api/app.js
@@ -7,6 +7,15 @@ const validateMailRouter = require('./routes/v0/validateMail')
 const usersMeRouter = require('./routes/v0/users/me')
 const app = express()
 
+const path = require('path')
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('*', function (req, res) {
+  res.sendFile('index.html', {
+    root: path.join(__dirname, 'build'),
+  })
+})
+
 require('dotenv').config()
 app.use(cors())
 app.use(express.json())
@@ -15,20 +24,7 @@ app.use('/users', usersRouter)
 app.use('/v0/authenticate', authenticateRouter)
 app.use('/v0/validateMail', validateMailRouter)
 app.use('/v0/users/me', usersMeRouter)
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404))
-})
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error')
-})
+console.log('app')
 
 module.exports = app
--- api/index.js
+++ api/index.js
@@ -2,6 +2,18 @@ const http = require('http')
 const app = require('./app')
 const server = http.createServer(app)
 
+///////
+// const express = require('express')
+// const app2 = express()
+// const path = require('path')
+// app2.use(express.static(path.join(__dirname, 'build')))
+// app2.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', index.html))
+// })
+// app2.listen(5000)
+
+///
+
 const { API_PORT } = process.env
 const port = process.env.PORT || API_PORT
 
